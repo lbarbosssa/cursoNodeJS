804ef22b5d3d9d1b4080bdbe7793fdb48bc8f5f2
Lucas Barbosa <barbosa.lucas1996@gmail.com>
2019-09-02 19:58:25 -0300

    HTTP - Abrindo um servidor

---
+++ HTTP/app.js
@@ -0,0 +1,16 @@
+/**
+ * chama o modulo = requre('http') .. padrao do node
+ * createServer = abre o servidor e cria sessão
+ * listen = porta de rede  onde vai abrir
+ * função de callback no create server, serve para receber e devolver respotas.
+ * esse metodo, é necessário parar o servidor para surtir efeito nas alterações
+ */
+
+var http = require('http')
+
+http.createServer((req, res) => {
+    res.end('Bem vindo ao website!')
+}).listen(8081)
+
+console.log('Servidor rodando')
+
